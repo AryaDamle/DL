b39b06e7194c366a30d6bf38f6876656b8e84557
Arya Damle <144161572+AryaDamle@users.noreply.github.com>
2025-04-11 07:46:36 +0000

    commit

--- App.py
+++ App.py
@@ -7,12 +7,12 @@ from API import transfer_style
 
 
 # Set page configs. Get emoji names from WebFx
-st.set_page_config(page_title="PixelMix - Style Transfer",
+st.set_page_config(page_title="Neural Style Transfer",
                    page_icon="./assets/favicon.png", layout="centered")
 
 # -------------Header Section------------------------------------------------
 
-title = '<p style="text-align: center;font-size: 50px;font-weight: 350;font-family:Cursive "> PixelMix </p>'
+title = '<p style="text-align: center;font-size: 50px;font-weight: 350;font-family:Cursive "> Neural Style Transfer </p>'
 st.markdown(title, unsafe_allow_html=True)
 
 
@@ -26,56 +26,6 @@ st.markdown(
 # Example Image
 st.image(image="./assets/nst.png")
 st.markdown("</br>", unsafe_allow_html=True)
-
-
-# -------------Sidebar Section------------------------------------------------
-
-
-with st.sidebar:
-
-    st.image(image="./assets/speed-brush.gif")
-    st.markdown("</br>", unsafe_allow_html=True)
-
-    st.markdown('<p style="font-size: 25px;font-weight: 550;">Some Inspiration 🎨</p>',
-                unsafe_allow_html=True)
-    st.markdown('Below are some of the art we created using PixelMix.',
-                unsafe_allow_html=True)
-
-    # ---------------------Example art images------------------------------
-
-    col1, col2 = st.columns(2)
-    with col1:
-        st.image(image="./assets/content1.jpg")
-    with col2:
-        st.image(image="./assets/art1.png")
-
-    col1, col2 = st.columns(2)
-    with col1:
-        st.image(image="./assets/content2.jpg")
-    with col2:
-        st.image(image="./assets/art2.png")
-
-    col1, col2 = st.columns(2)
-    with col1:
-        st.image(image="./assets/content3.jpg")
-    with col2:
-        st.image(image="./assets/art3.png")
-
-    col1, col2 = st.columns(2)
-    with col1:
-        st.image(image="./assets/content4.jpg")
-    with col2:
-        st.image(image="./assets/art4.png")
-
- # ----------------------------------------------------------------------
-
-    # line break
-    st.markdown(" ")
-    # About the programmer
-    st.markdown("## Made by **Deepesh Mhatre** \U0001F609")
-    st.markdown("Contribute to this project at "
-                "[*github.com/deepeshdm*](https://github.com/deepeshdm/Realtime_Face_Detection)")
-
 
 # -------------Body Section------------------------------------------------
 
@@ -92,8 +42,6 @@ with col2:
 
 
 st.markdown("</br>", unsafe_allow_html=True)
-st.warning('NOTE : You need atleast Intel i3 with 8GB memory for proper functioning of this application. ' +
-   ' Images greater then (2000x2000) are resized to (1000x1000).')
 
 
 if content_image is not None and style_image is not None:
@@ -112,9 +60,7 @@ if content_image is not None and style_image is not None:
 
         # output image
         styled_image = transfer_style(content_image, style_image, model_path)
-        if style_image is not None:
-            # some baloons
-            st.balloons()
+        
 
         col1, col2 = st.columns(2)
         with col1:
